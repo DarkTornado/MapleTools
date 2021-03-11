43eca344017fce72f21d027e672938d0df6ec2a0
Dark Tornado <dt3141592@naver.com>
2021-03-11 20:50:09 +0900

    Add files via upload

---
+++ java/com/darktornado/mapletools/CharActivity.java
@@ -0,0 +1,86 @@
+package com.darktornado.mapletools;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class CharActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#F58801")));
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+        WebView web = new WebView(this);
+        LinearLayout.LayoutParams margin = new LinearLayout.LayoutParams(-1, -1);
+        int mar = dip2px(20);
+        margin.setMargins(mar, mar, mar, mar);
+        web.setLayoutParams(margin);
+        layout.addView(web);
+        loadMapleInfo(getIntent().getStringExtra("name"), web);
+
+        layout.setBackgroundColor(Color.WHITE);
+        setContentView(layout);
+    }
+
+    private void loadMapleInfo(final String name, final WebView web) {
+        new Thread(() -> {
+            try {
+                Elements data0 = Jsoup.connect("https://maplestory.nexon.com/Ranking/World/Total?c=" + name).get().select("tr.search_com_chk");
+                String url = "https://maplestory.nexon.com" + data0.select("a").attr("href");//.replace(name + "?p=", name + "?Ranking=");
+                Document data = Jsoup.connect(url).get();
+
+                String img = data0.select("span.char_img").select("img").get(0).attr("src");
+                String rank = data0.select("td").get(0).select("p").get(0).text();
+                String exp = data0.select("td").get(3).text();
+                String pri = data0.select("td").get(4).text();
+                String guild = data0.select("td").get(5).text();
+                String serIcon = data0.select("img").get(5).attr("src");
+
+                Elements tmp = data.select("div.char_info").select("dd");
+                String lv = tmp.get(0).text().replace("LV.", "");
+                String job = tmp.get(1).text();
+                String serName = tmp.get(2).text();
+
+                final StringBuilder result = new StringBuilder("<meta name='viewport' content='user-scalable=no width=device-width' />")
+                        .append("<style>td{padding:5px;}table{border: 1px solid #000000;border-collapse: collapse;}</style>")
+                        .append("<table width=100% border=1><tr align=center><td style='padding:4px;'><b>닉네임</b></td><td>" + name + "</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>직업</b></td><td>" + job + "</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>레벨</b></td><td>" + lv + "</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>경험치</b></td><td>" + exp + "</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>순위</b></td><td>" + rank + "위</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>인기도</b></td><td>" + pri + "</td></tr>")
+                        .append("<tr align=center><td style='padding:4px;'><b>서버</b></td><td><img src='" + serIcon + "'>&nbsp;" + serName + "</td></tr>");
+                if (!guild.equals("")) {
+                    result.append("<tr align=center><td style='padding:4px;'><b>길드</b></td><td>" + guild + "</td></tr>");
+                }
+                result.append("<tr align=center><td style='padding:4px;'><b>캐릭터</b></td><td><img src='" + img + "'></td></tr>")
+                        .append("</table>");
+
+                runOnUiThread(() -> web.loadData(result.toString(), "text/html; charset=UTF-8", null));
+            } catch (Exception e) {
+                toast("해당 닉네임을 가진 캐릭터를 찾을 수 없거나, 오류가 발생했어요.");
+//                toast(e.toString());
+            }
+        }).start();
+    }
+
+
+    public int dip2px(int dips) {
+        return (int) Math.ceil(dips * this.getResources().getDisplayMetrics().density);
+    }
+
+    public void toast(final String msg) {
+        runOnUiThread(() -> Toast.makeText(CharActivity.this, msg, Toast.LENGTH_SHORT).show());
+    }
+
+}
---
+++ java/com/darktornado/mapletools/MainActivity.java
@@ -0,0 +1,102 @@
+package com.darktornado.mapletools;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#F58801")));
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+
+        WebView web = new WebView(this);
+        web.addJavascriptInterface(new JSLinker(), "android");
+        web.loadUrl("file:///android_asset/index.html");
+        web.getSettings().setJavaScriptEnabled(true);
+        layout.addView(web);
+
+        layout.setBackgroundColor(Color.WHITE);
+        setContentView(layout);
+    }
+
+    public void onButtonClick(int type) {
+        switch (type) {
+            case 0:
+                inputCharName();
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    private void inputCharName() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+        TextView txt1 = new TextView(this);
+        txt1.setText("캐릭터 이름 : ");
+        txt1.setTextColor(Color.BLACK);
+        txt1.setTextSize(18);
+        layout.addView(txt1);
+        final EditText txt2 = new EditText(this);
+        txt2.setHint("캐릭터 이름을 입력하세요...");
+        txt2.setSingleLine(true);
+        layout.addView(txt2);
+        int pad = dip2px(16);
+        layout.setPadding(pad, pad, pad, pad);
+        ScrollView scroll = new ScrollView(this);
+        scroll.addView(layout);
+        dialog.setView(scroll);
+        dialog.setTitle("캐릭터 정보 조회");
+        dialog.setNegativeButton("취소", null);
+        dialog.setPositiveButton("확인", (dialog1, whichButton) -> {
+            String name = txt2.getText().toString();
+            if (name.equals("")) {
+                toast("캐릭터의 이름이 입력되지 않았어요.");
+            } else {
+                Intent intent = new Intent(MainActivity.this, CharActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+                toast("캐릭터 정보를 불러오고 있어요...");
+            }
+        });
+        dialog.show();
+    }
+
+
+    public int dip2px(int dips) {
+        return (int) Math.ceil(dips * this.getResources().getDisplayMetrics().density);
+    }
+
+    public void toast(final String msg) {
+        runOnUiThread(() -> Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show());
+    }
+
+    private class JSLinker {
+        @JavascriptInterface
+        public void callNative(final int type) {
+            new Handler().post(() -> onButtonClick(type));
+        }
+
+    }
+    
+}
