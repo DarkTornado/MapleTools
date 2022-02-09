9f9ee77432101b0d87185001aded0e4e57801486
Dark Tornado <dt3141592@naver.com>
2022-02-09 13:38:25 +0900

    중복 제거
    분명 소스 수정하고 커밋했는데;;

--- java/com/darktornado/mapletools/MainActivity.java
+++ java/com/darktornado/mapletools/MainActivity.java
@@ -21,6 +21,10 @@ import android.widget.Toast;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     private WebView web;
@@ -131,26 +135,22 @@ public class MainActivity extends Activity {
 
     private void loadMesoPrice() {
         try {
-            String[][] unit = {
+            String[][] _unit = {
                     {"생수", "병"},
                     {"물", "L"},
-                    {"무릎통증 진료", "회"},
-                    {"인삼", "뿌리"},
-                    {"1 :", "으로 싸운 썰 듣기"},
                     {"딱밤", "대"},
+                    {"무릎통증 진료", "회"},
                     {"어제 산에서 찾은 산삼", "뿌리"},
-                    {"마카롱", "개"},
-                    {"라면", "봉지"},
-                    {"무토", "판"},
-                    {"머리카락", "가닥"},
-                    {"군생활", "일"},
-                    {"장미꽃", "송이"},
-                    {"감자", "개"},
+                    {"유통기한 하루 남은 라면", "봉지"},
+                    {"어제 빠진 머리카락", "가닥"},
+                    {"맛있는 민물장어", "마리"},
                     {"이리나가 깎은 감자", "개"},
-                    {"싸대기", "대"},
-                    {"장미꽃", "송이"},
-                    {"로얄 가드 성공", "번"}
+                    {"로얄 가드 성공", "번"},
+                    {"제로투 ", "시간 추기"},
+                    {"1 :", "으로 싸운 썰 듣기"}
             };
+            List<String[]> unit = Arrays.asList(_unit);
+            Collections.shuffle(unit);
             String data0 = Jsoup.connect("https://commapi.gamemarket.kr/comm/graph")
                     .ignoreContentType(true)
                     .post().text();
@@ -162,9 +162,8 @@ public class MainActivity extends Activity {
             JSONObject data1 = data.getJSONArray("reverse").getJSONObject(0);
             JSONObject data2 = data.getJSONArray("reverse2").getJSONObject(0);
             for (int n = 0; n < names.length; n++) {
-                int r = (int) (Math.random() * unit.length);
                 result.append("<tr align=center><td rowspan=2 bgcolor=#EEEEEE><b>" + names[n] + "</b></td><td>" + data1.getString("server" + (n + 1)) + " 메이플포인트</td></tr>")
-                        .append("<tr align=center><td>" + unit[r][0] + " " + data2.getString("server" + (n + 1)) + " " + unit[r][1] + "</td></tr>");
+                        .append("<tr align=center><td>" + unit.get(n)[0] + " " + data2.getString("server" + (n + 1)) + " " + unit.get(n)[1] + "</td></tr>");
             }
             result.append("</table>");
             Intent intent = new Intent(MainActivity.this, MesoActivity.class);
